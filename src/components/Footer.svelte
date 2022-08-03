aa14902568d5b53242e5ce2cf1f0227cae57750e
Saptak Das <saptak.das625@gmail.com>
2022-08-03 16:14:29 -0400

    Added Footer

--- src/components/Footer.svelte
+++ src/components/Footer.svelte
@@ -1,4 +1,22 @@
 <!--Footer-->
 <footer class="p-2">
     <hr>
+    <div class="row w-50 mx-auto text-center">
+        <div class="col col-lg-4">
+            Copyright © 2021 3 Tiny Bones. All rights reserved.
+        </div>
+        <div class="col">
+        <div class="row">
+            <div class="col">
+                Volunteer
+            </div>
+            <div class="col">
+                Privacy Policy
+            </div>
+            <div class="col">
+                FAQs
+            </div>
+        </div>
+        </div>
+    </div>
 </footer>
